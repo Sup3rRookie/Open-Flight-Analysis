6ae80ed120bfb1d2e6d8ffbb41fb33005c9b0123
Sup3rRookie <dev.akaun@gmail.com>
2021-09-30 22:09:14 +0800

    Count and List all airports in Malaysia

--- src/main.py
+++ src/main.py
@@ -27,9 +27,20 @@ with open('airports.csv','r', encoding='utf-8') as f:
     to_db = [(i['AirportID'],i['Name'], i['City'],i['Country'], i['IATA'],i['ICAO'], i['Latitude'],i['Longitude'], i['Altitude'], i['Timezone'],i['DST'], i['Tzdatabasetimezone'], i['Type'], i['Source']) for i in dr]
 cursor.executemany("INSERT INTO 'Open Flights' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", to_db)
 
-""" Alternative way to ingest csv data to db"""
+""" Other ways to ingest csv data to db"""
 # df = pd.read_csv('airports.csv')
 # df.to_sql('Open Flights', conn, if_exists='append', index=False)
 
+""" Analysis """
+def list_of_malaysia_airports():
+    result = cursor.execute("SELECT * from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
+    return result.fetchall()
+
+def count_all_airports_in_malaysia():
+    result = cursor.execute("SELECT DISTINCT count(*) from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
+    return result.fetchall()
+
+# list_of_malaysia_airports()
+# count_all_airports_in_malaysia()
 conn.commit()
 conn.close()
