50a04ad39f16fec48080eed24fb75cd16e54e1b8
Sup3rRookie <dev.akaun@gmail.com>
2021-10-01 11:22:59 +0800

    Determine the distance between the Malaysia Airports

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import sqlite3, csv, pandas as pd
+from geopy.distance import geodesic
+from functools import lru_cache
 
 conn= sqlite3.connect('airport.db',  timeout=10)
 cursor = conn.cursor()
@@ -32,15 +34,36 @@ cursor.executemany("INSERT INTO 'Open Flights' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?
 # df.to_sql('Open Flights', conn, if_exists='append', index=False)
 
 """ Analysis """
-def list_of_malaysia_airports():
-    result = cursor.execute("SELECT * from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
-    return result.fetchall()
+@staticmethod
+def list_of_malaysia_airports() -> None:
+	''' Display all airports in Malaysia.'''
+	results = cursor.execute("SELECT * from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
+	return results.fetchall()
 
-def count_all_airports_in_malaysia():
-    result = cursor.execute("SELECT DISTINCT count(*) from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
-    return result.fetchall()
+@staticmethod
+def count_all_airports_in_malaysia() -> None:
+	''' Total airports in Malaysia.'''
+	results = cursor.execute("SELECT DISTINCT count(*) from OpenFlights WHERE Country = 'Malaysia' ORDER by 'City' LIMIT 120")
+	return results.fetchall()
 
+@lru_cache(maxsize=None)
+def distance_between_each_airports() -> None:
+	'''Determine the distance in between the Malaysia airports'''
+	results = cursor.execute("SELECT Name,City,Latitude, Longitude from OpenFlights WHERE Country = 'Malaysia' order by 'City' LIMIT 120")
+	airports = list(results)
+	first_airport = (airports[0][-2], airports[0][-1])
+	source = airports[0][0]
+
+	for airport in airports:
+		destination = airport[0]
+		second_airport = (airport[-2], airport[-1])
+		total_distance = f'{(geodesic(first_airport, second_airport).km):.2f}'
+		result = print(f'From {source} to {destination}. The distance is {total_distance}km')
+	return result
+
+""" Function called"""
 # list_of_malaysia_airports()
 # count_all_airports_in_malaysia()
+distance_between_each_airports()
 conn.commit()
 conn.close()
